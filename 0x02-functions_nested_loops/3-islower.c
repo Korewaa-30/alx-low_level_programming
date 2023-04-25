397f617aeff7e0011b94716663643daf8dea6cb3
Korewaa-30 <dukersarah30@gmail.com>
2023-04-25 07:38:33 -0700

    Lower case c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 # include "main.h"
 
 /**
- * islower - Checks for lowercase character
+ * _islower - Checks for lowercase character
  * @c: The character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
 
-int islower(int c)
+int _islower(int c)
 
 {
 	if (c >= 97 && c <= 122)
