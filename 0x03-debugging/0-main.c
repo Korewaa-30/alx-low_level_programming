eec5a5b2ba94f6e7a25c64ee225ef8ee9a01222c
Korewaa-30 <dukersarah30@gmail.com>
2023-04-26 08:10:59 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,17 @@
 #include "main.h"
 
 /**
- * main - determine if number is positive, negative, or ero
- * 0 is the number to be checked
- * Return: 0 on success
+ * main - Test function for positive or negative
+ * Return: 0
  */
 
-void positive_or_negative(int i)
+int main(void)
 
 {
-	if (i < 0)
-	{
-	printf("%d is %s\n", i, "negative");
-	}
-	else if (i > 0)
-	{
-	printf("%d is %s\n", i, "positive");
-	}
-	else
-	{
-	printf("%d is %s\n", i, "zero");
-	}
-	return (0);
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
 }
