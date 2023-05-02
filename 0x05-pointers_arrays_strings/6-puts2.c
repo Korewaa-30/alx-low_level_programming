70354a6c2373e0f3fa0acb8cfe2e0bcd0904d5a0
Korewaa-30 <dukersarah30@gmail.com>
2023-05-02 12:04:21 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints characters of a string
+ * @str: the used string reference
+ * return: 0
+ */
+
+void puts2(char *str)
+{
+	int z = 0;
+
+	while (str[z] != '\0')
+	{
+		if (z % 2 == 0)
+		{
+			putchar(str[z]);
+		}
+		z++;
+	}
+	putchar('\n');
+}
