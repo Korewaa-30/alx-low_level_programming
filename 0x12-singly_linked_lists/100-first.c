96275de82a4551829b9aada25e4efa73d48cee88
Korewaa-30 <dukersarah30@gmail.com>
2023-06-01 14:54:35 -0700

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * sentence - prints a statement before the main function is executed
+ */
+
+void sentence(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
